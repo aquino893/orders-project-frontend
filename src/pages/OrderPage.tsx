161725e568779358f98c4c3fa38aa689fea46128
aquino893 <doris.aquino893@gmail.com>
2025-05-02 19:53:06 -0600

    maps

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -2,12 +2,17 @@ import { ToolbarActions } from '../components/OrderForm/ToolbarActions'
 import { OrderFormFields } from '../components/OrderForm/OrderFormFields'
 import { OrderLines } from '../components/OrderForm/OrderLines'
 import { ValidatedAddress } from '../components/OrderForm/ValidatedAddress'
+//import { LoadScript } from '@react-google-maps/api'
 
+//const GOOGLE_MAPS_API_KEY = 'AIzaSyCjHveQ_q2vYbkPKEVMMLFhqbvitRNdDoY'
 export default function OrderPage() {
   return (
     <div className="p-6 space-y-6">
       <ToolbarActions />
-      <OrderFormFields />
+     
+        <OrderFormFields />
+
+      
       <OrderLines />
       <ValidatedAddress />
     </div>
